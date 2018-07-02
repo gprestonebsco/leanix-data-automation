41f56077c5df47e122497ec2a40b9fb51d915e5b
Graham Preston <gpreston@ebsco.com>
2018-07-02 11:16:30 -0400

    automation1 security

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # LeanIX Data Automation
 1.	IT Components associated with Behavior should also be associated with the Behavior Provider
 2.	Data Objects associated with Behavior should also be associated with the Behavior Provider
-3.	IT Component Owners should also be the Bounded Context Owners (Persona objects)
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,9 @@ public class Main {
 
     List<Map<String, Object>> edgeList = (List<Map<String, Object>>) mainData.get("allFactSheets").get("edges");
 
+    // Keep track of newly created relations to not create them twice
+    List<String> editedItComponents = new ArrayList<String>();
+
     // Iterate through all behaviors
     for (Map<String, Object> edge : edgeList) {
       System.out.println("------------------------------------------------------------------\n");
@@ -25,67 +28,75 @@ public class Main {
               node.get("relInterfaceToProviderApplication");
       List<Map<String, Object>> applications = (List<Map<String, Object>>)
               relInterfaceToProviderApplication.get("edges");
-      Map<String, Object> behaviorProviderNode = (Map<String, Object>) applications.get(0).get("node");
-      Map<String, Object> behaviorProviderFactSheet = (Map<String, Object>) behaviorProviderNode.get("factSheet");
 
-      String behaviorProviderDisplayName = (String) behaviorProviderFactSheet.get("displayName");
-      String behaviorProviderId = (String) behaviorProviderFactSheet.get("id");
+      if (applications.size() > 0) {
+        Map<String, Object> behaviorProviderNode = (Map<String, Object>) applications.get(0).get("node");
+        Map<String, Object> behaviorProviderFactSheet = (Map<String, Object>) behaviorProviderNode.get("factSheet");
 
-      System.out.println("Behavior Provider: " + behaviorProviderDisplayName + " (" + behaviorProviderId + ")");
+        String behaviorProviderDisplayName = (String) behaviorProviderFactSheet.get("displayName");
+        String behaviorProviderId = (String) behaviorProviderFactSheet.get("id");
 
-      // Look through IT Components to see if they are each associated with the Behavior Provider
-      Map<String, Object> relInterfaceToITComponent = (Map<String, Object>) node.get("relInterfaceToITComponent");
-      List<Map<String, Object>> itComponents = (List<Map<String, Object>>) relInterfaceToITComponent.get("edges");
+        System.out.println("Behavior Provider: " + behaviorProviderDisplayName + " (" + behaviorProviderId + ")");
 
-      System.out.println("\nIT Components contain Behavior Provider:");
-      for (Map<String, Object> itEdge : itComponents) {
-        Map<String, Object> itNode = (Map<String, Object>) itEdge.get("node");
-        Map<String, Object> itFactSheet = (Map<String, Object>) itNode.get("factSheet");
-        Map<String, Object> relITComponentToApplication = (Map<String, Object>)
-                itFactSheet.get("relITComponentToApplication");
-        List<Map<String, Object>> itApplications = (List<Map<String, Object>>)
-                relITComponentToApplication.get("edges");
+        // Look through IT Components to see if they are each associated with the Behavior Provider
+        Map<String, Object> relInterfaceToITComponent = (Map<String, Object>) node.get("relInterfaceToITComponent");
+        List<Map<String, Object>> itComponents = (List<Map<String, Object>>) relInterfaceToITComponent.get("edges");
 
-        // Try to find Behavior Provider ID in the IT Component's Bounded Countexts
-        boolean containsBehaviourProviderId = false;
-        for (Map<String, Object> applicationEdge : itApplications) {
-          Map<String, Object> applicationNode = (Map<String, Object>) applicationEdge.get("node");
-          Map<String, Object> applicationFactSheet = (Map<String, Object>) applicationNode.get("factSheet");
-          if (applicationFactSheet.get("id").equals(behaviorProviderId)) {
-            containsBehaviourProviderId = true;
-            break;
+        System.out.println("\nIT Components contain Behavior Provider:");
+        for (Map<String, Object> itEdge : itComponents) {
+          Map<String, Object> itNode = (Map<String, Object>) itEdge.get("node");
+          Map<String, Object> itFactSheet = (Map<String, Object>) itNode.get("factSheet");
+          Map<String, Object> relITComponentToApplication = (Map<String, Object>)
+                  itFactSheet.get("relITComponentToApplication");
+          List<Map<String, Object>> itApplications = (List<Map<String, Object>>)
+                  relITComponentToApplication.get("edges");
+
+          // Try to find Behavior Provider ID in the IT Component's Bounded Countexts
+          boolean containsBehaviourProviderId = false;
+          for (Map<String, Object> applicationEdge : itApplications) {
+            Map<String, Object> applicationNode = (Map<String, Object>) applicationEdge.get("node");
+            Map<String, Object> applicationFactSheet = (Map<String, Object>) applicationNode.get("factSheet");
+            if (applicationFactSheet.get("id").equals(behaviorProviderId)) {
+              containsBehaviourProviderId = true;
+              break;
+            }
           }
-        }
 
-        System.out.println("* " + itFactSheet.get("displayName") + " (" + itFactSheet.get("id") + "): "
-                + containsBehaviourProviderId);
+          String itComponentId = (String) itFactSheet.get("id");
+          String itComponentDisplayName = (String) itFactSheet.get("displayName");
+          boolean editNeeded = !(containsBehaviourProviderId || editedItComponents.contains(itComponentId));
 
-        // If IT Component is not related to the Behavior Provider, create the relation
-        if (!containsBehaviourProviderId) {
-          // Get the revision number
-          Map<String, String> revIds = new HashMap<String, String>();
-          revIds.put("id", (String) itFactSheet.get("id"));
+          System.out.println("* " + itComponentDisplayName + " (" + itComponentId + "): "
+                  + !editNeeded);
 
-          Query revQuery = new Query(apiClient, "src/main/resources/rev.graphql", revIds);
-          Map<String, Map<String, Object>> revData = revQuery.execute();
+          // If IT Component is not related to the Behavior Provider, create the relation
+          if (editNeeded) {
+            // Get the revision number
+            Map<String, String> revIds = new HashMap<String, String>();
+            revIds.put("id", itComponentId);
 
-          String rev = revData.get("factSheet").get("rev").toString();
+            Query revQuery = new Query(apiClient, "src/main/resources/rev.graphql", revIds);
+            Map<String, Map<String, Object>> revData = revQuery.execute();
 
-          // Make the mutation
-          Map<String, String> mutationIds = new HashMap<String, String>();
-          mutationIds.put("itcomponentid", (String) itFactSheet.get("id"));
-          mutationIds.put("rev", rev);
-          mutationIds.put("providerid", behaviorProviderId);
+            String rev = revData.get("factSheet").get("rev").toString();
 
-          Query mutationQuery = new Query(apiClient, "src/main/resources/mutation1.graphql", mutationIds);
-          Map<String, Map<String, Object>> mutationData = mutationQuery.execute();
+            // Make the mutation
+            Map<String, String> mutationIds = new HashMap<String, String>();
+            mutationIds.put("itcomponentid", itComponentId);
+            mutationIds.put("rev", rev);
+            mutationIds.put("providerid", behaviorProviderId);
 
-          if (mutationData != null) {
-            System.out.println("  >>> Relation between " + itFactSheet.get("displayName")
-                    + " and " + behaviorProviderDisplayName + " added.");
-          }
-          else {
-            System.out.println("  >>> WARNING: No response for relation mutation .");
+            Query mutationQuery = new Query(apiClient, "src/main/resources/mutation1.graphql", mutationIds);
+            Map<String, Map<String, Object>> mutationData = mutationQuery.execute();
+
+            if (mutationData != null) {
+              editedItComponents.add(itComponentId);
+              System.out.println("  >>> Relation between " + itComponentDisplayName
+                      + " and " + behaviorProviderDisplayName + " added.");
+            }
+            else {
+              System.out.println("  >>> WARNING: No response for relation mutation .");
+            }
           }
         }
       }
@@ -146,11 +157,12 @@ public class Main {
               break;
             }
           }
+
           String dataObjectId = (String) dataFactSheet.get("id");
           String dataObjectDisplayName = (String) dataFactSheet.get("displayName");
           boolean editNeeded = !(containsBehaviourProviderId || editedDataObjects.contains(dataObjectId));
 
-          System.out.println("* " + dataFactSheet.get("displayName") + " (" + dataObjectId + "): "
+          System.out.println("* " + dataObjectDisplayName + " (" + dataObjectId + "): "
                   + !editNeeded);
 
           // If Data Object is not related to the Behavior Provider, create the relation
